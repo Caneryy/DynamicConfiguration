b08690df476b817a7ed2ebce483aeb0a391ea72c
Caner Yakupoglu <caneryakupoglu@gmail.com>
2020-02-09 22:48:57 +0300

    Added ServiceB project. Multiple Startup Projects

--- ServiceB/Controllers/HomeController.cs
+++ ServiceB/Controllers/HomeController.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using DynamicConfiguration.Model;
 using DynamicConfigurator;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ServiceA.Models;
+using ServiceB.Models;
 
-namespace ServiceA.Controllers
+namespace ServiceB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         public ConfigurationReader _configurationReader;
-
         public HomeController(ILogger<HomeController> logger, ConfigurationReader configurationReader)
         {
             _logger = logger;
